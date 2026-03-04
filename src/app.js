8f21e973c1d7649bd33f3f53f44ffec241f16603
Victor Manuel Hdz <victortomas567@gmail.com>
2026-03-04 12:57:25 -0600

    actualiza nodemon a la versión 3.1.14, agrega cors como middleware y crea archivo de configuración para Vercel

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 //! importamos la libreria
 import express from 'express'
 import dontenv from 'dotenv'
+import cors from 'cors'
 import gruposRoutes from './routes/grupos.routes.js'
 
 //! creamos el objeto de espress para nuestra aplicacion
@@ -14,6 +15,7 @@ const port = process.env.PORT || 3000;
 
 //! Implementar un middleware para poder implementar el formato JSON en las solicitudes
 app.use(express.json());
+app.use(cors())
 
 //! Rutas
 app.use('/api/grupos', gruposRoutes);
@@ -30,4 +32,4 @@ app.listen(port, ()=>{
     console.log(`Aplicacion corriendo en el puerto ${port}`)
 })
 
-console.log("Aplicacion iniciada correctamente");
+console.log("Aplicacion iniciada correctamente, siempre vivos");
